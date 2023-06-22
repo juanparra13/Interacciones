56a1fd7db4f4d037c0bac516d820980b4ca270bb
juanparra13 <134460487+juanparra13@users.noreply.github.com>
2023-06-22 12:10:03 -0500

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+function circulo(){
+    const figura = document.getElementById("figura");
+
+    figura.classList.toggle("circulo");
+}
+
+function rectangulo(){
+    const elemento = document.getElementById("figura");
+
+    elemento.classList.toggle("rectangulo");
+}
+
+function degradado(){
+    const degra = document.getElementById("figura");
+
+    degra.classList.toggle("degradado");
+}
+
+function ondas(){
+    const onda = document.getElementById("figura");
+    
+    onda.classList.toggle("ondas")
+}
+
+function gif(){
+    const gif = document.getElementById("figura");
+
+    gif.classList.toggle("gif");
+}
+
+function rombo(){
+    const rombo = document.getElementById("figura");
+
+    rombo.classList.toggle("rombo");
+}
+
+function triangulo(){
+    const triangulo = document.getElementById("figura");
+
+    triangulo.classList.toggle("triangulo");
+}
+
+function panel(){
+    const panel = document.getElementById("figura");
+
+    panel.classList.toggle("panel");
+}
+
+function panelS(){
+    const panelS = document.getElementById("figura");
+
+    panelS.classList.toggle("panelS");
+}
+
+function fondo(){
+    const fondo = document.getElementById("figura");
+
+    fondo.classList.toggle("fondo");
+}
